52019cd6c464d40b1a7f796a5014a3f9df0a3c64
LS-KR <Elihuso@outlook.com>
2024-11-09 08:33:28 +0000

    Deploying to gh-pages from @ LS-KR/MeowLanguage-Reborn@b637e833b7a158456f1ae27d9865ffcd5eff5585 🚀

---
+++ assets/About-3T3u8hKN.js
@@ -0,0 +1,9 @@
+var l=Object.defineProperty;var u=(a,n,e)=>n in a?l(a,n,{enumerable:!0,configurable:!0,writable:!0,value:e}):a[n]=e;var p=(a,n,e)=>u(a,typeof n!="symbol"?n+"":n,e);import{V as c,C as i,_,c as f,o as h}from"./index-CD4Ltv6R.js";const m=`<p>nya. nya~ 喵! 喵呜! 喵？ 喵呜！ 喵喵？ nya！ 喵呜? nya！ <em>(:з」∠)</em></p>
+<p>这是奇奇怪怪的喵语翻译器</p>
+<p>它什么用都没有</p>
+<ul>
+<li><a href="https://github.com/LS-KR/MeowLanguage-Reborn">Github</a></li>
+<li><a href="https://github.com/MeowBot233/MeowLanguageTranslator">Original</a></li>
+</ul>
+<p>喵喵! nya. 喵呜？ 喵喵。 喵喵? 喵喵! 喵喵! 喵喵! 喵喵! 喵喵! 喵喵! 喵喵! 喵喵! 喵喵! 喵喵! 喵喵! nya! 喵喵! 喵喵？ 喵。 喵喵! 喵喵. 喵喵~ nya。 喵呜。 喵～ nya~ 喵呜。 喵. nya. 喵. 喵喵？ 喵? 喵喵。 喵呜。 nya~ 喵呜? 喵呜! 喵喵. 喵呜！ 喵喵。 喵喵？ nya？ nya. 喵呜？ 喵～ 喵喵! 喵喵～ 喵呜~ 喵？ 喵呜? 喵喵! 喵呜~ 喵喵。 喵. 喵？ 喵呜~ 喵喵? nya. nya~ 喵~ 喵～ 喵～ 喵呜！ 喵? 喵喵。 喵呜～ nya～ 喵呜？ 喵呜！ 喵呜。 喵。 喵呜. 喵～ nya~ nya。 喵~ nya. 喵~ 喵？ 喵. 喵喵～ 喵喵! 喵喵～ 喵喵? 喵喵。 喵~ 喵～ 喵呜～ 喵喵! 喵呜。 nya? 喵呜. nya！ 喵喵! nya！ 喵喵？ nya！ nya？ 喵喵！ 喵呜？ 喵呜！ 喵喵～ 喵喵～ 喵? 喵～ nya？ 喵喵. 喵呜. 喵呜. 喵. 喵喵。 nya~ 喵喵！ nya。 nya! 喵. 喵~ nya? 喵？ 喵～ 喵呜～ nya。 喵呜. nya. 喵！ 喵呜～ 喵! 喵喵! 喵喵! 喵喵! 喵喵! Σ(っ°Д°;)っ</p>
+`;var b=Object.defineProperty,g=Object.getOwnPropertyDescriptor,v=(a,n,e,o)=>{for(var t=o>1?void 0:o?g(n,e):n,r=a.length-1,y;r>=0;r--)(y=a[r])&&(t=(o?y(n,e,t):y(t))||t);return o&&t&&b(n,e,t),t};let s=class extends c{constructor(){super(...arguments);p(this,"html",m)}};s=v([i({})],s);const d=["innerHTML"];function w(a,n,e,o,t,r){return h(),f("div",{class:"markdown",innerHTML:a.html},null,8,d)}const $=_(s,[["render",w]]);export{$ as default};
